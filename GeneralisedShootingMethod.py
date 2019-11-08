49eae9fe84c478391aee77aa69458e528963dff8
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2019-11-08 21:33:04 +0000

    Generalised shooting method working with Hopf Bifurcation

--- GeneralisedShootingMethod.py
+++ GeneralisedShootingMethod.py
@@ -1,7 +1,13 @@
 from scipy.optimize import newton
 from scipy.integrate import odeint
 
-def shooting(dXdt, X0, t, boundary_vars, tol=1e-4, maxiter=50):
+#define function to pass to solve
+def _func_to_solve(X, dXdt, t, boundary_vars):
+    """Return the difference between the shot and the target            conditions"""
+    sol=boundary_vars - odeint(dXdt,X,t)[-1]
+    return sol
+
+def solve(dXdt, X0, t, boundary_vars, tol=1e-4, maxiter=50):
     """
     A function that returns an estimation of the starting condition of a BVP subject to the first order differential equations
 
@@ -19,11 +25,6 @@ def shooting(dXdt, X0, t, boundary_vars, tol=1e-4, maxiter=50):
     #TODO add in comment about what happens in the numerical root finder fails
     #TODO ask Dr Barton what sort of additional options would be worth including
     #TODO what exactly is meant by the phase condition why might dcxdt dt be better than an x value
-     #define function to pass to solve
-     def _func_to_solve(X, dXdt, t, boundary_vars):
-     """Return the difference between the shot and the target        conditions"""
-     sol=boundary_vars - odeint(dXdt,X,t)[-1]
-     return sol
 
     # use the newton method to solve for the initial conditions
     return newton(_func_to_solve,X0,args=(dXdt,t,boundary_vars),tol=tol, maxiter=maxiter)
--- TestGeneralisedShootingMethod.py
+++ TestGeneralisedShootingMethod.py
@@ -7,19 +7,12 @@ from scipy.integrate import odeint
 import GeneralisedShootingMethod
 import numpy as np
 
+"""params such [a,b,d] below should be defined outside the function as it means one can write loops with varying params during numerical analysis"""
 #define params
-a=1
-d=0.1
-b=0.2
-
-#expected starting params at boundary
-boundary_vars=[0.38, 0.38]
-
-#time range
-t=np.linspace(0,20.76,10000)
+a=1; d=0.1; b=0.2
 
 # rate of change and pred and prey populations
-def dXdt(X,t=0):
+def dXdt_lotka_volterra(X,t=0):
      """Return the change in pred and prey populations"""
      return np.array([X[0]*(1-X[0])-((a*X[0]*X[1])/(d+X[0])),
                   b*X[1]*(1-(X[1]/X[0]))])
@@ -27,6 +20,34 @@ def dXdt(X,t=0):
 #define an initial guess for the starting conditions
 X0=[0.51,0.5]
 
+#expected starting params at boundary
+boundary_vars=[0.38, 0.38]
+
+#time range
+t=np.linspace(0,20.76,10000)
+
 #calc the result using a secant method
-res=GeneralisedShootingMethod.solve(dXdt, X0, t, boundary_vars)
+res=GeneralisedShootingMethod.solve(dXdt_lotka_volterra, X0, t, boundary_vars)
 print(res)#returns [0.37231387 0.37548837]
+
+
+
+"""
+Testing out generalised shooting method on the Hopf Bifurcation
+"""
+sigma=-1;beta=1
+def dXdt_hopf_bif(X, t=0):
+    """Function to calculate the rate of change of the Hopf Bifurcation at position X"""
+    return np.array([
+            beta*X[0]-X[1]+sigma*X[0]*(X[0]**2+X[1]**2),
+            X[0]+beta*X[1]+sigma*X[1]*(X[0]**2+X[1]**2)
+            ])
+
+# Initial conditions guess
+X0=[1,0]
+# Boundary Conditions
+boundary_vars=[0,1]
+# time range
+t=np.linspace(0,360,100)
+res=GeneralisedShootingMethod.solve(dXdt_hopf_bif, X0, t,          boundary_vars)
+print(res)#returns [ 15484.77502465 -58041.44113902 ]
